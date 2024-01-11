b9fbcda72aae56e36d10f3012dd444fb9842dd26
Jonathan Chen <jonathan.chen@uwa.edu.au>
2024-01-11 13:37:06 +0800

    1

--- DNAC-SWIM/dnac_config.py
+++ DNAC-SWIM/dnac_config.py
@@ -1,5 +1,5 @@
 import os
-DNAC=os.environ.get('DNAC','is-dnac-001.net.uwa.edu.au')
-DNAC_PORT=os.environ.get('DNAC_PORT',443)
-#DNAC_USER=os.environ.get('DNAC_USER','admin')
-#DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','Cisco123$')
+DNAC=os.environ.get('DNAC','sandboxdnac2.cisco.com')
+DNAC_PORT=os.environ.get('DNAC_PORT',8080)
+DNAC_USER=os.environ.get('DNAC_USER','devnetuser')
+DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','Cisco123!')
